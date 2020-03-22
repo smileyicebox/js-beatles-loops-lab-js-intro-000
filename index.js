3dbcc14d1e4cbc208bc94881e49d2b0c780993af
Jon Lewis <lewis.jonathan.n@gmail.com>
2020-03-22 17:00:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  solutionArray = []
+  for (let i = 0; i < musicians.length; i++) {
+    solutionString = `${musicians[i]} plays ${instruments[i]}`
+    solutionArray.push(solutionString)
+  }
+  return solutionArray
+}
